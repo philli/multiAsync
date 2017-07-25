c3c976f4dc84f596b05a18d15f98c455d24bf1ea
philli <philli@users.noreply.github.com>
2017-07-25 08:08:35 +0800

    Update multiAsync.js

--- multiAsync.js
+++ multiAsync.js
@@ -5,59 +5,52 @@
 
 (function(root, undefined) {
 
-    var multiAsync = (function() {
+    var multiAsync = function(tasks, callback) {
 
-        var index = 0;
-        var asyncs = {};
+        var isArrTasks = Object.prototype.toString.call(tasks) === '[object Array]';
 
-        return function(tasks, callback) {
+        if (tasks && (isArrTasks && tasks.length || typeof tasks === 'object') && typeof callback === 'function') {
+            var res;
+            var length = 0;
+            var count = 0;
 
-            var isArrTasks = Object.prototype.toString.call(tasks) === '[object Array]';
-
-            if (tasks && (isArrTasks && tasks.length || typeof tasks === 'object') && typeof callback === 'function') {
-                var res;
-                var length = 0;
-                var count = 0;
-
-                // 数组
-                if (isArrTasks) {
-                    res = [];
-                    length = tasks.length;
-                    while (tasks.length) {
-                        complete(res, length - tasks.length, tasks.shift());
-                    }
+            // 数组
+            if (isArrTasks) {
+                res = [];
+                length = tasks.length;
+                while (tasks.length) {
+                    complete(res, length - tasks.length, tasks.shift());
                 }
-                // 对象
-                else {
-                    res = {};
-                    // 单独获取length，避免非异步任务
-                    for (var k in tasks) {
-                        length++;
-                    }
-                    for (var k in tasks) {
-                        complete(res, k, tasks[k]);
-                    }
+            }
+            // 对象
+            else {
+                res = {};
+                // 单独获取length，避免非异步任务
+                for (var k in tasks) {
+                    length++;
                 }
+                for (var k in tasks) {
+                    complete(res, k, tasks[k]);
+                }
+            }
 
-                function complete(res, k, task) {
-                    if (typeof task === 'function') {
-                        task(function(data) {
-                            res[k] = data;
-                            if (++count === length) {
-                                callback(res);
-                            }
-                        });
-                    } else {
-                        res[k] = undefined;
+            function complete(res, k, task) {
+                if (typeof task === 'function') {
+                    task(function(data) {
+                        res[k] = data;
                         if (++count === length) {
                             callback(res);
                         }
+                    });
+                } else {
+                    res[k] = undefined;
+                    if (++count === length) {
+                        callback(res);
                     }
                 }
             }
-        };
-
-    })();
+        }
+    };
 
     'undefined' !== typeof module ? module.exports = multiAsync: root.multiAsync = multiAsync;
 
